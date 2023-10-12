332e7847cab523e41ef8015b7fc35bcd8fcae6b5
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2023-10-12 18:39:17 +0530

    [wip] - Arrays basics

---
+++ app.js
@@ -0,0 +1,24 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// const moreNumbers = new Array(); // Empty array
+// console.log(moreNumbers);
+
+// const moreNumbers = new Array('Hello', 'Welcome'); // length will be 2
+// console.log(moreNumbers);
+
+// const moreNumbers = new Array(1, 5, 3); // length will be 3
+// console.log(moreNumbers);
+
+// const moreNumbers = new Array(5); // fixed length of 5 but empty array
+// console.log(moreNumbers);
+
+// const moreNumbers = Array(5); // without new keyword fixed length of 5 but empty array
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 2); // of method to create array with specified values
+// console.log(yetMoreNumbers);
+
+// const moreNumbers = Array.from(12, 34); // This will not work
+const moreNumbers = Array.from(12, 34);
+console.log(moreNumbers);
