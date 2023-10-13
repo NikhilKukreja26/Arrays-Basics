8f5cb738deca90575872b2eb0dc86bdbf4e5c600
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2023-10-13 10:27:55 +0530

    [feature] - Different ways of creating arrays - Different data type storing in arrays

--- app.js
+++ app.js
@@ -20,5 +20,30 @@ console.log(numbers);
 // console.log(yetMoreNumbers);
 
 // const moreNumbers = Array.from(12, 34); // This will not work
-const moreNumbers = Array.from(12, 34);
-console.log(moreNumbers);
+// const moreNumbers = Array.from('Hii!');
+// console.log(moreNumbers); // ['H', 'i', 'i', '!']
+
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems); // Array from method will convert array like object or iterable in to real array
+for (const li of arrayListItems) {
+  console.log(li.textContent);
+}
+
+const hobbies = ['Playing Games', 'Coding'];
+const personalData = ['Nikhil', 27, { moreData: [] }];
+
+const analyticsData = [
+  [1.2, 5],
+  [3, 6],
+  [4, 8],
+];
+
+for (const data of analyticsData) {
+  for (const dataPoints of data) {
+    console.log(dataPoints);
+  }
+}
+
+console.log(personalData[1]);
